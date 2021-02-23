743e559beed309da50b1299c5472977db90f815f
Victor <doubletimeonly@gmail.com>
2021-02-22 22:05:53 -0500

    modified disjoint set to use cv::Point2i

--- DiceOCR/DisjointSet.cpp
+++ DiceOCR/DisjointSet.cpp
@@ -1,52 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include "opencv2/core/core.hpp"
 
 class DisjointSet
 {
-    std::unordered_map<int, int> parent;    // stores each node in set and their parent
-    std::unordered_map<int, int> rank;    // store the rank of each node
+    std::unordered_map<cv::Point2i, cv::Point2i> parent;    // stores each node in set and their parent
+    std::unordered_map<cv::Point2i, int> rank;    // store the rank of each node
 
 public:
     
-    void makeset(std::vector<int> const& universe)
+    void makeset(cv::Mat const& image)
     {
-        // initialize each value in vector as its own parent
-        for (int i : universe) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-    }
+        // initialize each pixel in image as its own parent
+        int rows = image.rows;
+        int cols = image.cols;
 
-    int findRoot(int k)
-    {
-        if (parent[k] != k)
+        for (int row = 0; row < rows; row++)
         {
-            parent[k] = findRoot(parent[k]);
+            for (int col = 0; col < cols; col++) 
+            {
+                cv::Point2i p(col, row);
+                parent[p] = p;
+                rank[p] = 0;
+            }
         }
-        return parent[k];
     }
 
-    void mergeSets(int x, int y)
+    cv::Point2i findRoot(cv::Point2i p)
     {
-        int xRoot = findRoot(x);
-        int yRoot = findRoot(y);
+        // path compression
+        // set parent of each node to the root of its respective set
+        if (parent[p] != p)
+        {
+            parent[p] = findRoot(parent[p]);
+        }
+        return parent[p];
+    }
+
+    void mergeSets(cv::Point2i p1, cv::Point2i p2)
+    {
+        cv::Point2i p1root = findRoot(p1);
+        cv::Point2i p2root = findRoot(p2);
 
         // merge the two sets based on their rank / depth
         // larger ranked set consunmes the lower ranked one
         // if they are the same, the depth has effectively increased
-        if (rank[xRoot] > rank[yRoot])
+        if (rank[p1root] > rank[p2root])
         {
-            parent[yRoot] = xRoot;
+            parent[p2root] = p1root;
         }
-        else if (rank[xRoot] < rank[yRoot])
+        else if (rank[p1root] < rank[p2root])
         {
-            parent[xRoot] = yRoot;
+            parent[p1root] = p2root;
         }
         else
         {
-            parent[xRoot] = yRoot;
-            rank[xRoot] += 1;
+            parent[p1root] = p2root;
+            rank[p1root] += 1;
         }
     }
 };
