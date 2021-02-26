e70ac24106deb5e69c108f74e18403fe9d449e6d
Victor <doubletimeonly@gmail.com>
2021-02-25 23:05:39 -0500

    added MergingTwoDisjointSetsShouldHaveNoErrors

--- test_disjointset/test.cpp
+++ test_disjointset/test.cpp
@@ -9,5 +9,44 @@ TEST(DisjointSetTests, CreatingDisjointSetFromGrayscaleImageShouldHaveNoErrors) 
     disjointset.makeset(grayscale_image);
 
     EXPECT_EQ(disjointset.getSetSize(), 4);
-    EXPECT_TRUE(true);
+
+    // check parents set properly
+    for (int row = 0; row < grayscale_image.rows; row++)
+    {
+        for (int col = 0; col < grayscale_image.cols; col++)
+        {
+            if (row == 0 && col == 0)
+            {
+                continue;
+            }
+            std::pair<int, int> p(row, col);
+            std::pair<int, int> parent = disjointset.findRoot(p);
+            EXPECT_EQ(parent, p);
+        }
+    }
+}
+
+TEST(DisjointSetTests, MergingTwoDisjointSetsShouldHaveNoErrors)
+{
+    DisjointSet disjointset;
+    cv::Mat grayscale_image = cv::Mat::ones(2, 2, CV_8U);
+    disjointset.makeset(grayscale_image);
+    
+    std::pair<int, int> origin(0, 0);
+    // check set merging and path compression
+    for (int row = 0; row < grayscale_image.rows; row++)
+    {
+        for (int col = 0; col < grayscale_image.cols; col++)
+        {
+            if (row == 0 && col == 0)
+            {
+                continue;
+            }
+            std::pair<int, int> p(row, col);
+            disjointset.mergeSets(origin, p);
+            std::pair<int, int> parent = disjointset.findRoot(p);
+            EXPECT_EQ(origin, parent);
+        }
+    }
+
 }
